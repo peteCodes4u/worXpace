6e25c2f94b2061db7d1ea5763ab4bb3d5ae858a0
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-05-20 21:59:50 -0400

    minor changes to startup

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 // terminal colors for messages
+const colors = require('colors');
 const green = '\x1b[32m%s\x1b[0m';
 const red = '\x1b[31m%s\x1b[0m';
 const yellow = '\x1b[33m%s\x1b[0m';
+const rainbow = colors.rainbow;
 
 
 // welcome ASCII art
 const figlet = require('figlet');
 
-figlet("worXspace", function(err, data){
+figlet("worXpace", function(err, data){
     if (err){
         console.log(red, "oops... something went wrong");
         console.dir(err);
         return;
     }
-    console.log(green, data);
+    console.log(rainbow(data));
+    
 });
+
+// prompts
+
+
+
 
 // initialize function
 function init() {
